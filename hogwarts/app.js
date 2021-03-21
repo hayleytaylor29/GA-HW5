4ef605d7e807ed7420f3fa7487a063f79b0d71e4
hayleytaylor29 <hft929@gmail.com>
2021-03-21 18:51:10 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -5,18 +5,40 @@
 // }
 $(() => {
     
-    const addContainer = () => {
-        const $container = '';
-        $('#container').append($container);
+    const $container = $('#container');
         console.log($container);
-    }
 
     const addH1 = () => {
         const $h1 = $('<h1>').text('Hogwarts');
         $('#container').append($h1);
         console.log($h1);
     }
-    addContainer();    
-    addH1();
+    const addH2 = () => {
+        const $h2 = $('<h2>').text('Hayley');
+        $('#container').append($h2);
+        console.log($h2);
+    }
+    const addH3 = () => {
+        const $h3 = $('<h3>').text('Hufflpuff');
+        $('#container').append($h3);
+        console.log($h3);
+    }
+    const addH4 = () => {
+        const $h4 = $('<h4 class="cat">').text('Jade');
+        $('#container').append($h4);
+        console.log($h4);
+    }
     
+    const addH41 = () => {
+        const $h4 = $('<h4>').text('Holly Wand with Unicorn Hair Core');
+        $('#container').append($h4);
+        console.log($h4);
+    }   
+    
+    addH1();
+    addH2();
+    addH3();
+    addH4();
+    addH41();
+
 });
