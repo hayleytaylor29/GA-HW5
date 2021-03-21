e235dd99aa50a79b808246375bb08878dd589466
hayleytaylor29 <hft929@gmail.com>
2021-03-21 18:06:08 -0400

    year 1 check

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,5 +1,19 @@
-if (typeof jQuery == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-}else{
-    console.log('I did it! I linked jQuery to this js file!');
-}
+// if (typeof jQuery == 'undefined'){
+//     console.log('oops! I still have to link my jQuery properly!');
+// }else{
+//     console.log('I did it! I linked jQuery to this js file!');
+// }
+$(() => {
+    const addContainer = () => {
+    const $container = $('#container').append($container);
+    console.log($container);
+    }
+
+    const addH1 = () => {
+        const $h1 = $('<h1>').text('Hogwarts');
+        $('#container').append($h1);
+        console.log($h1);
+    }
+        addH1();
+    
+});
