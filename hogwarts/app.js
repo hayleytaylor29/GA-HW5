061c0a84813b0c8b9641f797e4cd8a67f23e0371
hayleytaylor29 <hft929@gmail.com>
2021-03-22 22:43:24 -0400

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos.

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -46,7 +46,47 @@ $(() => {
         );
     }
     
+    const addH5 = () => {
+        const $h5 = $('<h5>').text('Spring 2017');
+        $('#container').append($h5);
+        console.log($h5);
+    }
 
+    //So I know there must be a way to make this shorter, and I wasn't able
+    //to get the borders right. I feel like I'm really close though!
+    const addTable = () =>{
+        const $table = $('<table>').addClass('table');
+        $('#container').append($table);
+        const $thead = $('<thead>');
+        $table.append($thead);
+        const $th1 = $('<th>').addClass('th').text('Day');
+        $thead.append($th1);
+        const $th2 = $('<th>').addClass('th').text('Classes');
+        $thead.append($th2);
+        const $tr1 = $('<tr>');
+        $thead.append($tr1);
+        const $td1 = $('<td>').addClass('td').text('Monday');
+        $tr1.append($td1);
+        const $td11 = $('<td>').addClass('td').text('Tuesday');
+        $td1.append($td11);
+        const $td12 = $('<td>').text('Wednesday');
+        $td11.append($td12);
+        const $td13 = $('<td>').text('Thursday');
+        $td12.append($td13);
+        const $td14 = $('<td>').text('Friday');
+        $td13.append($td14);
+        const $td2 = $('<td>').text('Herbology');
+        $tr1.append($td2);
+        const $td3 = $('<td>').text('Divination');
+        $td2.append($td3);
+        const $td4 = $('<td>').text('History of Magic');
+        $td3.append($td4);
+        const $td5 = $('<td>').text('Charms');
+        $td4.append($td5);
+        const $td6 = $('<td>').text('Potions');
+        $td5.append($td6);
+        console.log($table);
+    }
 
     addH1();
     addH2();
@@ -54,4 +94,6 @@ $(() => {
     addH4();
     addH41();
     addList();
+    addH5();
+    addTable();
 });
